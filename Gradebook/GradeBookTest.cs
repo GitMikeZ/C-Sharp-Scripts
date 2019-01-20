0c226848bd1a6aba9c2eb8a0edc73dffa566515c
Mike <GitMikeZ@users.noreply.github.com>
2019-01-19 21:21:19 -0800

    Uploaded GradeBook scripts
    -Uses jagged array of scores and calculates the averages, min, and max

---
+++ Gradebook/GradeBookTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class GradeBookTest
+{
+    static void Main(string[] args)
+    {
+        int[,] gradesArray = { {87, 97, 70},
+                               {62, 87, 90},
+                               {100, 81, 82},
+                               {83, 62, 85},
+                               {73, 87, 65},
+                               {85, 75, 83},
+                               {91, 90, 100},
+                               {79, 72, 84},
+                               {87, 93, 73}};
+
+        GradeBook myGradeBook = new GradeBook(
+            "CS101 Introduction to C# Programming", gradesArray);
+        myGradeBook.DisplayMessage();
+        myGradeBook.ProcessGrades();
+    }
+}
+
